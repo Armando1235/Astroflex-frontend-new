7d9e9e081494ed30a66e5fafbd2bbc235ee78acd
Armando Ghekiere <armando.ghekiere@gmail.com>
2021-03-27 00:33:01 +0400

    about page

--- src/About.js
+++ src/About.js
@@ -3,11 +3,32 @@ import React from 'react';
 const About = () => {
     return(
         <div>
-        <h1>This is the about page</h1>
+            <div class="row align-items-center">
+                <h2>What is AstroFlex?</h2>
+                <p>AstroFlex is a technology company that connects people with the best in their cities.</p>
+                <p>We do this by empowering local businesses and in turn, generate new ways for people to earn, work and live.</p>
+                <p>Sounds too good to be true? Well, get started and see it happening!</p>
+            </div>
+            <div>
+                <h2>Our mission</h2>
+                <p>Our mission is to enable anyone to have anything delivered on demand.</p>
+                <p>Our goal is to transform the way goods move around the city while transforming the lives of our flex drivers by providing them additional income.</p>
+                <p>Through our revolutionary app that connects customers with local couriers who can deliver nearly anything from your favorite restaurant or retailer in minutes.</p>
+            </div>
+            <div>
+                <h2>Our start</h2>
+                <p>AstroFlex started 2years ago, with an objective of delivering packages for our dear customers.</p>
+            </div>
+            <div>
+                <h2>Legal</h2>
+                <p>AstroFlex is a company registered at the Dubai Department of Economic Development.</p>
+            </div>
+
+
+
         </div>
     )
 }
 
 export default About;
-
 
